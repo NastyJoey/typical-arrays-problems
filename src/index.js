f6394421fb722c1295a487224d0089845b298198
NastyJoey <59571074+NastyJoey@users.noreply.github.com>
2020-03-12 17:06:13 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    return Array.isArray(array) && array.length > 0
+        ? array.reduce((acc, elem) => (acc > elem ? elem : acc))
+        : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return Array.isArray(array) && array.length > 0
+        ? array.reduce((acc, elem) => (acc < elem ? elem : acc))
+        : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length > 0
+        ? array.reduce((acc, elem) => acc + elem) / array.length
+        : 0;
+};
